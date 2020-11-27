880866f37083ebf7be36b326753bbceb8261d324
AronDerenyi <aron.derenyi@gmail.com>
2020-11-27 16:52:12 +0100

    Explicitly added the webpack config to the build-app script

--- app/index.ts
+++ app/index.ts
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import App from "./src/view/App.vue";
+import App from "src/view/App.vue";
 
 const agentRegex = /.*/;
 const agentSupported = agentRegex.test(navigator.userAgent);
