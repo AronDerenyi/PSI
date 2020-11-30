b956968c2fbd6ccc6b5707fcdc5e1c49ffac94bd
AronDerenyi <aron.derenyi@gmail.com>
2020-11-30 11:06:08 +0100

    Added express-session to manage user sessions

---
+++ Server/app/src/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const bodyParser = require('body-parser');
+const session = require('express-session')
+
+// Creating the Server
+
+const app = express();
+
+// Setting up Express
+
+app.use(session({
+	name: "sid",
+	secret: "L9QCCmn82N",
+	resave: false,
+	saveUninitialized: true,
+	cookie: {
+		secure: false,
+		maxAge: 1000 * 60 * 60
+	},
+	store: session.MemoryStore()
+}))
+
+app.use(bodyParser.urlencoded({extended: true, limit: '5mb'}));
+app.use(bodyParser.json({extended: true, limit: '5mb'}));
+
+app.use(express.static('../Client/dist'));
+
+// Starting the Server
+
+app.listen(process.env.PORT || 80);
