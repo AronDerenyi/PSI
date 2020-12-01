9768b9b53cd61f0d7493898a3a0d5ffb8ec9551d
AronDerenyi <aron.derenyi@gmail.com>
2020-12-01 12:12:16 +0100

    Added the dummy views and models to the client

---
+++ Client/app/src/view/InputOptions.vue
@@ -0,0 +1,18 @@
+<template>
+	<h1>{{viewModel.title}}</h1>
+</template>
+
+<script lang="ts">
+	import {Vue, Component} from "vue-property-decorator";
+	import {AppModel} from "../viewmodel/AppModel";
+
+	@Component
+	export default class InfoVideo extends Vue {
+
+		private viewModel = new AppModel()
+	};
+</script>
+
+<style scoped>
+
+</style>
---
+++ Client/app/src/viewmodel/InfoImageModel.ts
@@ -0,0 +1,4 @@
+export class AppModel {
+
+	readonly title: string = "Hello World!"
+}
