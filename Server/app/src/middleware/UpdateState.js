92bf94aa642b22546efc2566d4e282b631486f2a
AronDerenyi <aron.derenyi@gmail.com>
2020-12-11 23:17:31 +0100

    Resetting the session now regenerates the session id

--- Server/app/src/middleware/UpdateState.js
+++ Server/app/src/middleware/UpdateState.js
@@ -2,14 +2,16 @@ module.exports = (test) => (req, res, next) => {
 	const session = req.session
 	const state = test.states[session.stateId]
 
+	// TODO: Save results
+	console.log(session.id)
+	console.log(session.results)
+
 	if (state.ending) {
-		console.log(session.results)
-		// TODO: Save results
-		session.stateId = null
-		session.results = null
+		req.session.regenerate(function(err) {
+			next(err)
+		})
 	} else {
 		session.stateId = state.next(session.results)
+		next()
 	}
-
-	next()
 };
