3e936a77feabb7d18882b4fd7b09e33cc03b8975
AronDerenyi <aron.derenyi@gmail.com>
2021-01-10 14:32:38 +0100

    Added character encoding to csv

--- Server/app/src/middleware/ReturnResultsCSV.js
+++ Server/app/src/middleware/ReturnResultsCSV.js
@@ -1,5 +1,5 @@
 module.exports = (converter) => (req, res) => {
-	res.setHeader('Content-Type', 'text/csv')
+	res.setHeader('Content-Type', 'text/csv; charset=utf-8')
 	res.setHeader('Content-Disposition', 'attachment; filename="results.csv"')
 	res.send(converter.toCSV(res.locals.results)).end()
 };
